ebaf0c98814ca55f49e4bf0c56c89a3edd9e1013
Thanh Nguyen <nnthanh1996@gmail.com>
2022-04-25 14:20:29 +0700

    Update filter for custom message content

--- app/code/Wdevs/CustomBar/Plugin/Magento/Customer/Controller/Adminhtml/Group/SavePlugin.php
+++ app/code/Wdevs/CustomBar/Plugin/Magento/Customer/Controller/Adminhtml/Group/SavePlugin.php
@@ -4,18 +4,36 @@
 namespace Wdevs\CustomBar\Plugin\Magento\Customer\Controller\Adminhtml\Group;
 
 
+use Magento\Cms\Model\Template\FilterProvider;
+
 class SavePlugin
 {
     /** @var \Magento\Customer\Model\GroupFactory $groupFactory */
     protected $groupFactory;
+    /**
+     * @var FilterProvider
+     */
+    protected $filterProvider;
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    protected $logger;
 
     /**
      * SavePlugin constructor.
      * @param \Magento\Customer\Model\GroupFactory $groupFactory
+     * @param FilterProvider $filterProvider
+     * @param \Psr\Log\LoggerInterface $logger
      */
-    public function __construct(\Magento\Customer\Model\GroupFactory $groupFactory)
+    public function __construct(
+        \Magento\Customer\Model\GroupFactory $groupFactory,
+        FilterProvider $filterProvider,
+        \Psr\Log\LoggerInterface $logger
+    )
     {
         $this->groupFactory = $groupFactory;
+        $this->filterProvider = $filterProvider;
+        $this->logger = $logger;
     }
 
     public function afterExecute(\Magento\Customer\Controller\Adminhtml\Group\Save $subject, $result) {
@@ -24,13 +42,20 @@ class SavePlugin
 
         $customMessage = $request->getParam('custom_message');
 
+        try {
+            $filteredCustomMessage = $this->filterProvider->getPageFilter()->filter($customMessage);
+        } catch (\Exception $e) {
+            $filteredCustomMessage = $customMessage;
+            $this->logger->error($e->getMessage());
+        }
+
         $id = $request->getParam('id');
 
         if($id != null) {
 
             $customerGroup = $this->groupFactory->create()->load($id);
 
-            $customerGroup->setData('custom_message', $customMessage);
+            $customerGroup->setData('custom_message', $filteredCustomMessage);
 
             $customerGroup->save();
 
